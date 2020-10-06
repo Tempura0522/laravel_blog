daf0ce558f0ff3281391316f452655fd32539ebe
Tempura <tempura0522@icloud.com>
2020-10-06 16:00:48 +0900

    パスワードリセット機能の実装

--- laravel_blog/routes/web.php
+++ laravel_blog/routes/web.php
@@ -3,13 +3,21 @@
 /*
 |--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
+|---------'-----------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+
+Route::get('/testmail', function(){
+    Mail::to('test@example.com')->send(new TestMail);
+    return 'メール送信完了';
+});
 
 Route::get('/', function () {
     return view('welcome');
